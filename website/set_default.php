9fb8e50d4aa59bd69b122946507363d8cf1bb147
Tim Strutzberg <56938835+dasBunny@users.noreply.github.com>
2020-04-03 13:59:45 +0200

    improved admin panel and prepared login

---
+++ website/set_default.php
@@ -0,0 +1,26 @@
+<?php
+    require 'connect_db.php';
+    $trackname="riefensbeek";
+     if(isset($_POST['trackname'])){
+        $trackname = $_POST['trackname']; 
+    }
+    $status="fail";
+    if($mysqli->connect_error) {
+        echo json_encode($status);
+        exit('Could not connect');
+    }
+    $stmt = mysqli_stmt_init($mysqli);
+    $query="UPDATE config SET value=? where property='default_track'";
+    if(!mysqli_stmt_prepare($stmt, $query)){
+    } else {
+      mysqli_stmt_bind_param($stmt,"s",$trackname);
+      if (mysqli_stmt_execute($stmt)) {
+        $result=mysqli_stmt_get_result($stmt);
+        $status="sucess";
+        }
+    }
+echo json_encode($status);
+
+mysqli_close($mysqli);
+
+?> 
